25bb70363bd9fbdd6a03f6b86954a3d12cbcee49
vasu0110 <vasu0110@gmail.com>
2025-02-05 17:48:55 -0800

    commit after error

--- terraformbasic/main.tf
+++ terraformbasic/main.tf
@@ -1,12 +1,32 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.85.0"
-    }
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  tags = {
+    Name    = "terraform-demo"
+    Purpose = "terraform-practice"
   }
 }
 
-provider "aws" {
-  # Configuration options
-}
+/* resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+} */
